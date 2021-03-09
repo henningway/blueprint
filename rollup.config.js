2259d7d424cee864738d2040423f088f85239331
Henning Schindler <henning.schindler@peerox.de>
2021-03-09 20:36:00 +0100

    rollup config: omit minification during development

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import filesize from 'rollup-plugin-filesize';
-import { terser } from 'rollup-plugin-terser';
 
-export default {
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default (async () => ({
     input: 'src/index.js',
     output: {
         name: 'Blueprint',
         file: 'dist/index.js',
         format: 'umd'
     },
-    plugins: [filesize(), terser()]
-};
+    plugins: [filesize(), isProduction && (await import('rollup-plugin-terser')).terser()]
+}))();
