57a12837579e31c4b2e522e489b9bef1b8466a5e
Henning Schindler <henning.schindler@peerox.de>
2022-01-30 18:55:48 +0100

    move from umd to cjs+esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,31 @@
+import pkg from './package.json';
 import filesize from 'rollup-plugin-filesize';
+import terser from 'rollup-plugin-terser';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default (async () => ({
-    input: 'src/index.js',
-    output: {
-        name: 'Blueprint',
-        file: 'dist/index.js',
-        format: 'umd'
+const input = 'src/index.js';
+
+const plugins = [nodeResolve(), filesize(), isProduction && terser.terser()];
+
+export default [
+    {
+        input,
+        output: {
+            file: pkg.module,
+            format: 'esm',
+            sourcemap: true
+        },
+        plugins
     },
-    plugins: [filesize(), isProduction && (await import('rollup-plugin-terser')).terser()]
-}))();
+    {
+        input,
+        output: {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true
+        },
+        plugins
+    }
+];
